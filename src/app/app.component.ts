f0b69699b7d2075fa5d7427b2d4e7917fb2a62c7
SergeyMell <sergey.mell@agilie.com>
2018-05-02 18:53:02 +0300

    Download and upload examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
@@ -10,11 +10,34 @@ export class AppComponent {
   title = 'app';
   posts: any;
 
-  constructor(http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts').subscribe(res => console.log(res));
-    http.get('https://jsonplaceholder.typicode.com/posts').subscribe(res => console.log(res));
-    http.get('https://jsonplaceholder.typicode.com/posts').subscribe(res => console.log(res));
-    http.get('https://jsonplaceholder.typicode.com/posts').subscribe(res => console.log(res));
+  file: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  downloadFile() {
+    this.http.get('http://api.mobile.design/uploads/shots/239d30d434bb80223407f9e375a64e6790bd9234.?1505722325', {responseType: 'blob'}).subscribe(res => {
+      const link = document.createElement('a');
+      const url = window.URL.createObjectURL(res);
+      link.href = url;
+      link.download = 'my-file.jpg';
+      link.click();
+      window.URL.revokeObjectURL(url);
+    });
+  }
+
+  getFile(event) {
+    this.file = event.target.files[0];
+  }
+
+  sendFile() {
+    const formData = new FormData();
+    formData.append('file', this.file);
+
+    this.http.post('https://file.io', formData).subscribe(res => {
+      console.log(res);
+    });
   }
 
   handleDropdownSelection(value) {
