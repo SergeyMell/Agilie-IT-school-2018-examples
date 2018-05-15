fc0fce9fad18352a8ca54fbee196827ae59cac3f
SergeyMell <sergey.mell@agilie.com>
2018-05-15 18:38:06 +0300

    ngFor experiments

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  data = {
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3'
+  };
+
+  dataIterable = {
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3',
+    [Symbol.iterator]() {
+      let index = 0;
+      const keys = Object.keys(this).filter(el => this.hasOwnProperty(el));
+      return {
+        next: () => {
+          const value = this[keys[index]];
+          const done = index >= keys.length;
+          index++;
+          return {value, done};
+        }
+      };
+    }
+  };
+
+  constructor() {
+    Object.prototype[Symbol.iterator] = function () {
+      let index = 0;
+      const keys = Object.keys(this).filter(el => this.hasOwnProperty(el));
+      return {
+        next: () => {
+          const value = this[keys[index]];
+          const done = index >= keys.length;
+          index++;
+          return {value, done};
+        }
+      };
+    };
+  }
 
   ngOnInit() {
+  }
+
+  toArray(object) {
+    return Object.keys(object)
+      .filter(el => object.hasOwnProperty(el));
   }
 
 }
